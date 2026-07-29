9feac2fcd4e84a7b29fb5bac6a843aeb9a3b57b1
hyuneey2 <hyuneey2@gmail.com>
2026-07-29 11:32:26 +0900

    프로젝트 페이지 추가

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -14,6 +14,7 @@ export function SiteHeader() {
         <nav className="site-nav" aria-label="주요 메뉴">
           <Link href="/">Home</Link>
           <Link href="/posts">Posts</Link>
+          <Link href="/projects">Projects</Link>
           <Link href="/about">About me</Link>
         </nav>
       </div>
